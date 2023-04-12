3915b07976bedcbb39bb2190e69a6bbe6672aa04
Shilpak Raich <shilpakraich91@gmail.ccom>
2023-04-12 14:32:05 +0200

    Updates

--- Day1.py
+++ Day1.py
@@ -1,1 +1,16 @@
-print("Hello World !!!");
+print("Hello World !!!")
+print('"Hello World !!!"')
+print("Hello World!\nhello\nworld")
+print("Hello" + " " + "Shilpak")
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+print("Hello " + input("What is you name \n") )
+
+print(len(input("What is your name ?")))
