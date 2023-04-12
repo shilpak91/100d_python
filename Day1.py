734bc0bbc582c679903eda8696b4b6c965c73829
Shilpak Raich <shilpakraich91@gmail.ccom>
2023-04-12 15:59:13 +0200

    Day 1

--- Day1.py
+++ Day1.py
@@ -1,16 +1,19 @@
-print("Hello World !!!")
-print('"Hello World !!!"')
-print("Hello World!\nhello\nworld")
-print("Hello" + " " + "Shilpak")
+# print("Hello World !!!")
+# print('"Hello World !!!"')
+# print("Hello World!\nhello\nworld")
+# print("Hello" + " " + "Shilpak")
 
-#Fix the code below 👇
+# #Fix the code below 👇
 
-print("Day 1 - String Manipulation")
-print('String Concatenation is done with the "+" sign.')
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with a backslash and n.")
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
 
+# print("Hello " + input("What is you name \n") )
 
-print("Hello " + input("What is you name \n") )
+# print(len(input("What is your name ?")))
 
-print(len(input("What is your name ?")))
+name = input("What is your name ?")
+length = len(name)
+print("Your name is {} and length is {}")
