26c8e4cb2fa444637815a9ba49a45568fe6e85b4
Shilpak Raich <shilpakraich91@gmail.ccom>
2023-05-16 14:29:05 +0200

    Update

--- Day20_Snake_Game/scoreboard.py
+++ Day20_Snake_Game/scoreboard.py
@@ -6,8 +6,16 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(x=0,y=280)
         self.hideturtle()
+        self.write_score()
+
+    def write_score(self):
+        self.clear()
+        self.goto(x=0,y=280)
         self.write("Score : ",move=False, align='left', font=('Arial', 8, 'normal'))
         self.goto(x=50,y=280)
         self.write(self.score,move=False, align='left', font=('Arial', 8, 'normal'))
+
+    def update_score(self):
+        self.score +=1
+        self.write_score()
