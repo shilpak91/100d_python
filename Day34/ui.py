a5955a2427aa0ec8f3e2937bd99d38e32733af04
Shilpak Raich <shilpakraich91@gmail.ccom>
2023-05-30 16:21:16 +0200

    Update

--- Day34/ui.py
+++ Day34/ui.py
@@ -1,2 +1,40 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
+
+
+
+
+class QuizInterface:
+    def __init__(self,quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20,pady=20,background=THEME_COLOR)
+
+        self.canvas = Canvas(width=300,height=250,highlightthickness=0,bg="white")
+        self.question_text = self.canvas.create_text(150,125,text="Some Question Text",fill=THEME_COLOR,font=("Arial",20,"italic"),width=280)
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
+        
+        self.score_label = Label(text="Score: 0",fg="white",bg=THEME_COLOR)
+        self.score_label.grid(row=0,column=1)
+
+        true_img = PhotoImage(file="C:\\Users\\RaichS01\\OneDrive - FrieslandCampina\\Documents\\Repos\\100d_python\\Day34\\images\\true.png")
+        false_img = PhotoImage(file="C:\\Users\\RaichS01\\OneDrive - FrieslandCampina\\Documents\\Repos\\100d_python\\Day34\\images\\false.png")
+
+        self.true_button = Button(image=true_img,highlightthickness=0)
+        self.true_button.grid(row=2,column=0)
+
+        self.false_button = Button(image=false_img,highlightthickness=0)
+        self.false_button.grid(row=2,column=1)
+
+        self.get_next_question()
+        
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text= self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text,text=q_text)
 
