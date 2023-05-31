7b30e85e2b0a22fa030c0644372b86af3e3acdc9
Shilpak Raich <shilpakraich91@gmail.com>
2023-05-31 20:39:01 +0200

    Update

--- Day35_RainAlert/main.py
+++ Day35_RainAlert/main.py
@@ -1,4 +1,5 @@
 import requests
+import datetime as dt
 
 api_key = "39SR6WKXA454E6Q67A6AQDZFL"
 
@@ -6,8 +7,29 @@ parameters = {
     "key" : api_key
 }
 
-response = requests.get(url="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Utrecht,Netherlands",params=parameters)
-print(response.json())
+today = dt.datetime.now()
+today_date = today.date()
+current_hour = int(today.hour)
+
+
+# print(current_hour)
+
+
+response = requests.get(url="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/21,124",params=parameters)
+next_hourly_data = response.json()["days"][0]["hours"][current_hour-1:]
+
+# print(next_hourly_data)
+is_rain = False
+
+for data in next_hourly_data:
+    if "Rain" in data["conditions"]:
+        is_rain = True
+
+if is_rain:
+    print("Bring and unbrella")
+    # We can also send a email here from smtb lib
+
+
 
 
 # https://api.openweathermap.org/data/2.5/weather?lat=52.0248&lon=5.0918&appid=8cc17e3bca0b1304370a5ccd50a96adc
