277640e96263fa2160bc6502133f92b3a778690a
Shilpak Raich <shilpakraich91@gmail.com>
2023-06-08 22:28:50 +0200

    update

--- Day39-FlightTracker/flight_search.py
+++ Day39-FlightTracker/flight_search.py
@@ -1,8 +1,11 @@
 import requests
+from flight_data import FlightData
 API_KEY = "pFWdhxXKElM55GBOJSc6Awme4KAbFkfQ"
 LOCATION_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
+search_endpoint = "https://api.tequila.kiwi.com/v2/search"
 
 location_search_header = {"apikey" : API_KEY}
+
 
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
@@ -11,3 +14,37 @@ class FlightSearch:
             response = requests.get(url=LOCATION_ENDPOINT,params=param ,headers=location_search_header)
             iata_code = response.json()["locations"][0]["code"]
             return iata_code
+    
+    def check_flights(self,origin_city_code, destination_city_code, from_time, to_time):
+        flight_search_header = {"apikey" : API_KEY}
+
+        query = {"fly_from": origin_city_code,
+                "fly_to": destination_city_code,
+                "date_from":from_time.strftime("%d/%m/%Y"),
+                "date_to": to_time.strftime("%d/%m/%Y"),
+                "nights_in_dst_from": 7,
+                "nights_in_dst_to": 28,
+                "flight_type": "round",
+                "one_for_city": 1,
+                "max_stopovers": 0,
+                "curr": "GBP"
+                }
+
+        response = requests.get(url=search_endpoint,params=query,headers=flight_search_header)
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+        
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
--- Day39-FlightTracker/main.py
+++ Day39-FlightTracker/main.py
@@ -2,11 +2,13 @@
 
 import data_manager as dm
 import flight_search as fs
+from datetime import datetime,timedelta
 
 data = dm.DataManager()
 dest_data = data.get_destination_data()
+flight_search = fs.FlightSearch()
 
-flight_data = fs.FlightSearch()
+ORIGIN_CITY_IATA = "LON"
 
 
 city_code_check = {i["city"]:{"iataCode": i["iataCode"],"row_num" : i["id"]} for i in dest_data}
@@ -14,6 +16,18 @@ city_code_check = {i["city"]:{"iataCode": i["iataCode"],"row_num" : i["id"]} for
 
 for city in city_code_check:
     if city_code_check[city]["iataCode"] == "":
-        iata_code = flight_data.get_destination_code(city)
+        iata_code = flight_search.get_destination_code(city)
         record = city_code_check[city]["row_num"]
         data.update_destination_code(iata_code,record)
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+
+for destination in dest_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
