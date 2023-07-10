3832005791582d704130d853cba6f8d4cea667f0
Shilpak Raich <shilpakraich91@gmail.com>
2023-07-10 21:01:19 +0200

    Update

---
+++ Web Development/bs4-start/main.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+
+with open("/Users/shilpak/Documents/Code100/100d_python/Web Development/bs4-start/website.html") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents,"html.parser")
+print(soup.title)
+print(soup.title.string)
+
+all_anchor_tag = soup.find_all(name="a")
+print(all_anchor_tag)
+
+for tag in all_anchor_tag:
+    print(tag.getText())
+    print(tag.get("href"))
+
+
+heading = soup.find(name="h1",id="name")
+print(heading)
