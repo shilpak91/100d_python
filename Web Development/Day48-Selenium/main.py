3ef8b82d0da5aadb1d19bdd932814941bc8d644e
Shilpak Raich <shilpakraich91@gmail.com>
2023-07-24 13:20:30 +0200

    Update

--- Web Development/Day48-Selenium/main.py
+++ Web Development/Day48-Selenium/main.py
@@ -16,59 +16,77 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID,value="cookie")
 
-start_time = datetime.now()
-end_time = start_time + timedelta(seconds=5)
+# start_time = datetime.now()
+# end_time = start_time + timedelta(seconds=5)
 
-start_time = start_time.strftime("%H:%M:%S")
-end_time = end_time.strftime("%H:%M:%S")
+# start_time = start_time.strftime("%H:%M:%S")
+# end_time = end_time.strftime("%H:%M:%S")
 
-game_on = True
-
-while game_on:
-    cookie.click()
-    game_end_time = datetime.now()
-    game_end_time = game_end_time.strftime("%H:%M:%S")
-    if end_time == game_end_time:
-        game_on = False
-    time.sleep(0.005)
-
-# Get Current cookie count
-current_cookie_count = driver.find_element(By.ID,value="money")
-current_cookie = int(current_cookie_count.text)
+timeout = time.time() + 5
+five_min = time.time() + 60*5 # 5minutes
 
 #Get upgrade item ids.
 items = driver.find_elements(By.CSS_SELECTOR,"#store div")
 item_ids = [item.get_attribute("id") for item in items]
 item_ids = item_ids[:-1]
 
-# Get all available upgrades
-all_avail_upgrades = driver.find_elements(By.CSS_SELECTOR,value="#store b")
-all_avail_upgrades = all_avail_upgrades[:-1]
-# print(all_avail_upgrades)
+game_on = True
 
-item_price = []
+while game_on:
 
-for item in all_avail_upgrades:
-    all_item = item.text.split("-")[1]
-    item_price.append(int(all_item.strip().replace(',','')))
+    cookie.click()
 
-# print(item_price)
+    # game_end_time = datetime.now()
+    # game_end_time = game_end_time.strftime("%H:%M:%S")
+    if time.time() > timeout:
+    #     game_on = False
+    # time.sleep(0.005)
+
+        # Get Current cookie count
+        current_cookie_count = driver.find_element(By.ID,value="money")
+        current_cookie = int(current_cookie_count.text.strip().replace(',',''))
+
+        # Get all available upgrades
+        all_avail_upgrades = driver.find_elements(By.CSS_SELECTOR,value="#store b")
+        all_avail_upgrades = all_avail_upgrades[:-1]
+        # print(all_avail_upgrades)
+
+        # Get price for upgrades
+        item_price = []
+        for item in all_avail_upgrades:
+            all_item = item.text.split("-")[1]
+            item_price.append(int(all_item.strip().replace(',','')))
+
+        # print(item_price)
+
+        #Create dictionary of store items and prices
+        cookie_upgrades = {}
+        for n in range(len(item_price)):
+            cookie_upgrades[item_price[n]] = item_ids[n]
+
+        # print(cookie_upgrades)
 
 
-# Checking max available upgrades
-affordable_upgrades = {}
-for n in range(len(item_price)):
-    if current_cookie > item_price[n]:
-        affordable_upgrades[item_ids[n]] = item_price[n]
+        # Checking max available upgrades
+        affordable_upgrades = {}
+        for cost,id in cookie_upgrades.items():
+            if current_cookie > cost:
+                affordable_upgrades[cost] = id
 
-# print(affordable_upgrades)
-max_affordable_value = max(affordable_upgrades.values())
+        # print(affordable_upgrades)
+        highest_price_affordable_upgrade = max(affordable_upgrades)
+        # print(highest_price_affordable_upgrade)
+        to_purchase_id = affordable_upgrades[highest_price_affordable_upgrade]
+
+        # Get most costly upgrade
+        driver.find_elements(By.ID,to_purchase_id)[0].click()
+
+        #Add another 5 seconds until the next check
+        timeout = time.time() + 5
 
 
-# Get most costly upgrade
-get_upgrade = [key for key, val in affordable_upgrades.items() if val == max_affordable_value]
-get_upgrade_id = get_upgrade[0]
-upgrade = driver.find_elements(By.ID,get_upgrade_id)
-upgrade[0].click()
-
-
+    #After 5 minutes stop the bot and check the cookies per second count.
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(By.ID,"cps").text
+        print(cookie_per_s)
+        game_on = False
